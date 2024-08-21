b75b129172127b89df2b94e209c7cd890cf1a791
JustinCHughes <hcjustin713@gmail.com>
2024-08-20 23:03:10 -0500

    Changed println to print and added \n at end of Hello World. Passed test.

--- src/main/java/com/csc/HelloWorld.java
+++ src/main/java/com/csc/HelloWorld.java
@@ -1,9 +1,7 @@
 package com.csc;
 
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!\n");
     }
 }
